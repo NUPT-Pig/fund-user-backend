342c21a3caca270bfa49f5f8a04366032cb434b9
anshun_zhu <nupt_b09040212@sina.com>
2020-12-21 22:25:59 +0800

    (function) [unit-test]
    {}

--- src/test/java/org/nuptpig/funduserbackend/FundUserBackendApplicationTests.java
+++ src/test/java/org/nuptpig/funduserbackend/FundUserBackendApplicationTests.java
@@ -2,12 +2,14 @@ package org.nuptpig.funduserbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FundUserBackendApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
